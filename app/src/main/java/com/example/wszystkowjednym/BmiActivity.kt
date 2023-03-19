0c87ea2af45b2c4d77becdf763e75b7f03e61c49
Agnieszka Szynal <agnieszka.szynal5@gmail.com>
2023-03-19 15:20:48 +0100

    create bmi and measurement activities

---
+++ app/src/main/java/com/example/wszystkowjednym/BmiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wszystkowjednym
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BmiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi)
+    }
+}
--- app/src/main/java/com/example/wszystkowjednym/MainActivity.kt
+++ app/src/main/java/com/example/wszystkowjednym/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.wszystkowjednym
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bmi_button.setOnClickListener {
+            var new_activity : Intent = Intent(applicationContext, BmiActivity::class.java)
+            startActivity(new_activity)
+        }
+
+        measurement_button.setOnClickListener {
+            var new_activity : Intent = Intent(applicationContext, MeasurementActivity::class.java)
+            startActivity(new_activity)
+        }
+
+
     }
 }
