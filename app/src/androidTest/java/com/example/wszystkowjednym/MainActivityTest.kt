6c767212ab4b0f5674399b278d8d987fe3682552
Agnieszka Szynal <agnieszka.szynal5@gmail.com>
2023-04-12 21:29:58 +0200

    editing activities and adding tests

---
+++ app/src/androidTest/java/com/example/wszystkowjednym/MainActivityTest.kt
@@ -0,0 +1,50 @@
+package com.example.wszystkowjednym
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = launchActivity()
+        scenario.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun is_main_activity_visible_test() {
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun open_activity_bmi_test() {
+        onView(withId(R.id.bmi_button)).perform(click())
+        onView(withId(R.id.activity_bmi)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun open_activity_bmi_test() {
+        onView(withId(R.id.measurement_button)).perform(click())
+        onView(withId(R.id.)).check(matches(isDisplayed()))
+    }
+
+
+
+
+
+}
