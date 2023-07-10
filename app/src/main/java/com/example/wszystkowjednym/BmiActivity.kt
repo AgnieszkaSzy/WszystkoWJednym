ba13da860899f522fd4646756f4aecfa4bceea43
Agnieszka Szynal <agnieszka.szynal5@gmail.com>
2023-07-10 19:28:27 +0200

    adding validation to field: gender, weight, height

--- app/src/main/java/com/example/wszystkowjednym/BmiActivity.kt
+++ app/src/main/java/com/example/wszystkowjednym/BmiActivity.kt
@@ -11,11 +11,28 @@ class BmiActivity : AppCompatActivity() {
         setContentView(R.layout.activity_bmi)
 
         calculate_button.setOnClickListener {
-            val weight = weight_input.text.toString().toDouble()
-            val height = height_input.text.toString().toDouble()
-            calculate_bmi(weight, height, bmi_result)
-            bmi_result_text.visibility = TextView.VISIBLE
-            infoBmiButton.visibility = TextView.VISIBLE
+
+            if ((female_button.isChecked or male_button.isChecked) && weight_input.length()>0 && height_input.length()>0) {
+                gender_error_info.visibility = TextView.INVISIBLE
+                weight_error_info.visibility = TextView.INVISIBLE
+                height_error_info.visibility = TextView.INVISIBLE
+                val weight = weight_input.text.toString().toDouble()
+                val height = height_input.text.toString().toDouble()
+                calculate_bmi(weight, height, bmi_result)
+                bmi_result_text.visibility = TextView.VISIBLE
+                infoBmiButton.visibility = TextView.VISIBLE
+            } else {
+                if (!female_button.isChecked and !male_button.isChecked) {
+                    gender_error_info.visibility = TextView.VISIBLE} else {
+                        gender_error_info.visibility = TextView.INVISIBLE}
+                if (weight_input.length() == 0) {
+                    weight_error_info.visibility = TextView.VISIBLE} else {
+                        weight_error_info.visibility = TextView.INVISIBLE}
+                if (height_input.length() == 0) {
+                    height_error_info.visibility = TextView.VISIBLE} else {
+                        height_error_info.visibility = TextView.INVISIBLE
+                    }
+            }
         }
 
         clear_data_button.setOnClickListener {
