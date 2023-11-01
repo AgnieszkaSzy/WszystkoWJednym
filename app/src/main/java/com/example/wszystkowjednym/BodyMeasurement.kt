21f231780cb55583abcd2bdc09b4eb8f70510d55
Agnieszka Szynal <agnieszka.szynal5@gmail.com>
2023-11-01 22:32:10 +0100

    measurement activity

---
+++ app/src/main/java/com/example/wszystkowjednym/BodyMeasurement.kt
@@ -0,0 +1,13 @@
+package com.example.wszystkowjednym
+
+import android.widget.EditText
+
+fun clearMeasurementData (Date: EditText, waist: EditText, hips: EditText, belly: EditText, thigh: EditText, chest: EditText, arm: EditText) {
+    Date.setText("")
+    waist.setText("")
+    hips.setText("")
+    belly.setText("")
+    thigh.setText("")
+    chest.setText("")
+    arm.setText("")
+}
--- app/src/main/java/com/example/wszystkowjednym/MeasurementActivity.kt
+++ app/src/main/java/com/example/wszystkowjednym/MeasurementActivity.kt
@@ -2,10 +2,16 @@ package com.example.wszystkowjednym
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bmi.*
+import kotlinx.android.synthetic.main.activity_measurement.*
 
 class MeasurementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_measurement)
+
+        clearMeasurementButton.setOnClickListener {
+            clearMeasurementData(measurementDate, waistMeasurementData, hipsMeasurementData, bellyMeasurementData, thighMeasurementData, chestMeasurementData, armMeasurementData)
+        }
     }
 }
